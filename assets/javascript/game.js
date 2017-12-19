3bb9b8e4726a1c9c4f6feba4bcfedaaf76db9e3d
unknown <aguirreh8@gmail.com>
2017-12-18 22:21:49 -0500

    added checkAnswers function, added submitButton fuctionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,33 +11,51 @@ const questions = [{
 	'10',
 	'21',
 	"34"],
-	rightAnswer: '44'
+	rightAnswer: '34'
 }]
 
 function addQuestions() {
 	for (let i = 0; i < questions.length; i++) {
 		const questionText = $('<p>');
-		const form = $('<form>');
-		const input = $('<input>');
 		let j = i + 1;
 		questionText.text(questions[i].question);
-		// $('#questions').append(questionText);
 		$('#questions').append(questionText);
 		for (x = 0; x < 4; x++) {
-			// input.attr({
-			// 	type:'radio',
-			// 	name:'question' + j,
-			// 	value: questions[i].choices[x]
-			// })
 			$('#questions').append('<input type="radio" name="question' + j + '" value="' + questions[i].choices[x] + '" />', questions[i].choices[x]);
 		}
 	}
 }
 
+function checkAnswers(answer1, answer2) {
+	const answerArray = [answer1, answer2];
+	let right = 0;
+	let wrong = 0;
+	let unanswered = 0;
+	for (let i = 0; i < answerArray.length; i++) {
+		if (answerArray[i] == questions[i].rightAnswer) {
+			right++;
+		} else if (answerArray[i] == undefined) {
+			unanswered++;
+		} else {
+			wrong++;
+		}
+	}
+	console.log("Right: " + right);
+	console.log("Wrong: " + wrong);
+	console.log("Unanswered: " + unanswered);
+	console.log("--------------")
+}
+
 $(document).ready(function() {
 	$('#startScreen').click(function() {
 		$('#startScreen').addClass('is-hidden');
-		$('#questions').removeClass('is-hidden');
+		$('#questionScreen').removeClass('is-hidden');
 		addQuestions();
+	})
+
+	$('#submitButton').click(function() {
+		let radioValue1 = $("input[name='question1']:checked").val();
+		let radioValue2 = $("input[name='question2']:checked").val();
+		checkAnswers(radioValue1, radioValue2);
 	})
 })
